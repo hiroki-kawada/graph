d88c41afee2017d6870f9eb9be5d166efbffffb9
川田広輝 <f13bc005@gmail.com>
2023-10-09 12:03:23 +0900

    初回コミット

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # graph
+
+### Canvasを使用
+- グラフを自作して表示するために「Canvas」を使用して表示する。
+
+### 主に使用する処理
+- drawCircle
+  - 円を描画する際に使用する処理
+    - color：表示する際の色
+    - radius：円の半径
+    - center：中心座標
+    - alpha：不透明度
+    - style：塗りつぶし指定
+    - colorFilter：フィルター？(使用する機会がないので不明)
+    - blendMode ：使用する機会がないので不明
+
+- drawPath
+  - 点と点を結びつけて図形を描画する処理
+    - path：描画するための点の情報を保存してある変数
+    - color：表示する際の色
+    - alpha：不透明度
+    - style：pathの部分を塗り潰すかの状態を変化
+    - colorFilter：フィルター？(使用する機会がないので不明)
+    - blendMode ：使用する機会がないので不明
+
+
+- drawRect
+  - 短形(四角)を描画する処理
+    - color：表示する際の色
+    - topLeft：描画開始位置
+    - size：描画時の大きさ
+    - alpha：不透明度
+    - style：塗りつぶし指定
+    - colorFilter：フィルター？(使用する機会がないので不明)
+    - blendMode ：使用する機会がないので不明
+
+- drawArc
+  - 弧を描画する処理
+    - color：表示する際の色
+    - startAngle：開始角度を設定（0は３時を表しています。）
+    - sweepAngle：startAngleに対して時計回りに描かれた円弧のサイズ
+    - useCenter：円弧が境界の中心を閉じるかどうかを示すフラグ(true：中心を閉じる、false：開始角度、終了角度)
+    - size：弧の寸法
+    - topLeft：描画開始位置
+    - alpha：不透明度
+    - style：塗りつぶし指定
+    - colorFilter：フィルター？(使用する機会がないので不明)
+    - blendMode ：使用する機会がないので不明
+
+
+- clipPath
+  - 指定したpathを使用して描画領域をクリップするための処理
+    - path：描画するための点の情報を保存してある変数
+    - clipOp：指定された境界線でクリッピング操作を行う（基本的にClipOp.Intersectで問題なし）
+
---
+++ app/src/main/java/com/example/graphapp/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.example.graphapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.ClipOp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.graphapp.ui.theme.GraphAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GraphAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LazyColumn {
+                        item { CircleCanvas() }
+                        item { RectCanvas() }
+                        item { PathCanvas() }
+                        item { ArcCanvas() }
+                        item { ClipPathCanvas() }
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CircleCanvas() {
+    Canvas(
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ) {
+        drawCircle(Color.Blue, radius = 20.dp.toPx())
+    }
+}
+
+@Composable
+fun RectCanvas() {
+    Canvas(
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ) {
+        drawRect(Color.Blue, topLeft = Offset(10F, 20f), size = size / 2F)
+    }
+}
+
+@Composable
+fun PathCanvas() {
+    Canvas(
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ) {
+
+        val path = Path()
+        path.moveTo(size.width, 0F)
+        path.lineTo(size.width, size.height)
+        path.lineTo(0F, size.height)
+        drawPath(path = path, Color.Blue)
+    }
+}
+
+
+@Composable
+fun ArcCanvas() {
+    Canvas(
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ) {
+        drawArc(Color.Blue, startAngle = 0f, sweepAngle = 90f, useCenter = true)
+    }
+}
+
+@Composable
+fun ClipPathCanvas() {
+    Canvas(
+        modifier = Modifier
+            .width(100.dp)
+            .height(100.dp)
+    ) {
+
+        val path = Path()
+        path.moveTo(size.width, 0F)
+        path.lineTo(size.width, size.height)
+        path.lineTo(0F, size.height)
+        clipPath(
+            path = path, clipOp = ClipOp.Intersect
+        ) {
+            // 三角形
+            drawPath(path = path, Color.Blue)
+
+            //クリップ用の短形
+            drawRect(
+                Color.Red, size = Size(
+                    0.5f * size.width,
+                    size.height
+                )
+            )
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CircleCanvasPreview() {
+    GraphAppTheme {
+        CircleCanvas()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RectCanvasPreview() {
+    GraphAppTheme {
+        RectCanvas()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PathCanvasPreview() {
+    GraphAppTheme {
+        PathCanvas()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ArcCanvasPreview() {
+    GraphAppTheme {
+        ArcCanvas()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ClipPathCanvasPreview() {
+    GraphAppTheme {
+        ClipPathCanvas()
+    }
+}
